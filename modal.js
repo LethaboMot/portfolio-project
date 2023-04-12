06bf2bcaa845b459f04b28f70efe6f69810c5699
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-04-12 23:20:16 +0200

    Push

--- modal.js
+++ modal.js
@@ -1,22 +1,18 @@
 const popup = document.getElementById('myPopup');
 const btn = document.getElementById('myBtn');
 const span = document.getElementsByClassName('close')[0];
-const modal = document.getElementById('myPopup1');
+
+const modalup = document.getElementById('myPopup1');
 const button = document.getElementById('myBtn1');
-const closeMenu = document.getElementsByClassName('close1')[0];
-const popup2 = document.getElementById('myPopup2');
-const btn2 = document.getElementById('myBtn2');
-const span2 = document.getElementsByClassName('close2')[0];
+const close = document.getElementsByClassName('close1')[0];
 
-function addPopupTrigger(btn2, popup2) {
-  btn2.onclick = function () {
-    popup2.style.display = 'block';
+const click = document.getElementById('myPopup2');
+const open = document.getElementById('myBtn2');
+const end = document.getElementsByClassName('close2')[0];
 
-    span.onclick = function () {
-      popup2.style.display = 'none';
-    };
-  };
-}
+const page = document.getElementById('myPopup3');
+const reveal = document.getElementById('myBtn3');
+const conceal = document.getElementsByClassName('close3')[0];
 
 btn.onclick = function () {
   popup.style.display = 'block';
@@ -26,16 +22,26 @@ span.onclick = function () {
   popup.style.display = 'none';
 };
 
-popup2();
-
 button.onclick = function () {
-  modal.style.display = 'block';
+  modalup.style.display = 'block';
 };
 
-closeMenu.onclick = function () {
-  modal.style.display = 'none';
+close.onclick = function () {
+  modalup.style.display = 'none';
 };
 
-btn2();
-span2();
-addPopupTrigger('');
+open.onclick = function () {
+  click.style.display = 'block';
+};
+
+end.onclick = function () {
+  click.style.display = 'none';
+};
+
+reveal.onclick = function () {
+  page.style.display = 'block';
+};
+
+conceal.onclick = function () {
+  page.style.display = 'none';
+};
