10929684a349002917246edd8c0b2a8ba8272268
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-04-12 19:07:28 +0200

    Updated the Javascript

--- modal.js
+++ modal.js
@@ -1,19 +1,32 @@
 const popup = document.getElementById('myPopup');
 const btn = document.getElementById('myBtn');
 const span = document.getElementsByClassName('close')[0];
-
-const popup1 = document.getElementById('myPopup1');
-const btn1 = document.getElementById('myBtn1');
-const span1 = document.getElementsByClassName('close1')[0];
-
+const modal = document.getElementById('myPopup1');
+const button = document.getElementById('myBtn1');
+const closeMenu = document.getElementsByClassName('close1')[0];
 const popup2 = document.getElementById('myPopup2');
 const btn2 = document.getElementById('myBtn2');
 const span2 = document.getElementsByClassName('close2')[0];
+const modalbuttons = document.querySelectorAll('.bluebutton');
 
-const popup3 = document.getElementById('myPopup3');
-const btn3 = document.getElementById('myBtn3');
-const span3 = document.getElementsByClassName('close3')[0];
 
+modalbuttons.forEach(function(btn) {
+  btn.onclick = function(){
+    const modal = btn.getAttribute('.bluebutton');
+
+    document.getElementById(modal).style.display = 'block';
+  };
+});
+
+function addPopupTrigger(btn, popup2) {
+  btn.onclick = function () {
+    popup2.style.display = 'block';
+
+    span.onclick = function () {
+      popup.style.display = 'none';
+    };
+  };
+}
 btn.onclick = function () {
   popup.style.display = 'block';
 };
@@ -22,66 +35,10 @@ span.onclick = function () {
   popup.style.display = 'none';
 };
 
-window.onclick = function (event) {
-  if (event.target === popup) {
-    popup.style.display = 'none';
-  }
+button.onclick = function () {
+  modal.style.display = 'block';
 };
 
-// second card
-
-btn1.onclick = function () {
-  popup1.style.display = 'block';
+closeMenu.onclick = function () {
+  popup.style.display = 'none';
 };
-
-span1.onclick = function () {
-  popup1.style.display = 'none';
-};
-
-window.onclick = function (event) {
-  if (event.target === popup1) {
-    popup1.style.display = 'none';
-  }
-};
-
-// third card
-
-btn2.onclick = function () {
-  popup2.style.display = 'block';
-};
-
-span2.onclick = function () {
-  popup2.style.display = 'none';
-};
-
-window.onclick = function (event) {
-  if (event.target === popup2) {
-    popup2.style.display = 'none';
-  }
-};
-
-//  fourth card
-
-btn3.onclick = function () {
-  popup3.style.display = 'block';
-};
-
-span3.onclick = function () {
-  popup3.style.display = 'none';
-};
-
-window.onclick = function (event) {
-  if (event.target === popup3) {
-    popup3.style.display = 'none';
-  }
-};
-
-// const modalbuttons = document.querySelectorAll('.bluebutton');
-
-// modalbuttons.forEach(function(btn) {
-//   btn.onclick = function(){
-//     const modal = btn.getAttribute('data-modal');
-
-//     document.getElementById(modal).style.display = 'block';
-//   };
-// });
